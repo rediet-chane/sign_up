c52c429e44903adc5723136b6f9aaaa2e6a2a8f7
Rediet <redichane123@gmail.com>
2026-07-28 11:04:55 +0300

    25

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -2,24 +2,32 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDjd8eiCDbwt9Qb2qEPdCyE_dWoOLYtlfY", // Get this from lib/firebase_options.dart
+  apiKey: "AIzaSyDjd8eiCDbwt9Qb2qEPdCyE_dWoOLYtlfY",
   authDomain: "sign-up-app-227a0.firebaseapp.com",
   projectId: "sign-up-app-227a0",
   storageBucket: "sign-up-app-227a0.appspot.com",
   messagingSenderId: "303868688064",
-  appId: "1:303868688064:web:1221b9c40981978f745f25" // Get this from lib/firebase_options.dart
+  appId: "1:303868688064:web:1221b9c40981978f745f25"
 });
 
 const messaging = firebase.messaging();
-messaging.usePublicVapidKey("BGoCuRuvSPmZak6PIX4zR4iXmUcgCWNgX0HQlecb8bJw6aUVYbRZYD79JEhkuGWYbn0cx9hghkuiaW5doubUHzM");
+
 messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
-  const notificationTitle = payload.notification.title || 'New Notification';
+  console.log(
+    '[firebase-messaging-sw.js] Received background message',
+    payload
+  );
+
+  const notificationTitle =
+      payload.notification?.title || 'New Notification';
+
   const notificationOptions = {
-    body: payload.notification.body || 'You have a new message.',
+    body: payload.notification?.body || 'You have a new message.',
     icon: '/icons/Icon-192.png',
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
 });
